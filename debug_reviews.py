dba6eae44e55936d35b7e9ebe18603ad26e709fc
Pamery Arredondo <pamery@validatedcs.com>
2026-03-11 12:24:11 -0400

    Fixed merge conflicts, added date parser, cleared old DB

--- debug_reviews.py
+++ debug_reviews.py
@@ -1,20 +1,30 @@
 import sqlite3
 import json
 
+# Connect to the database
 con = sqlite3.connect("reviews.db")
 con.row_factory = sqlite3.Row
 cur = con.cursor()
 
+# Query reviews that actually have an owner response
 cur.execute("""
-    SELECT author, owner_responses
+    SELECT author, review_date, owner_response_date, owner_responses
     FROM reviews
     WHERE owner_responses != '{}'
     AND owner_responses IS NOT NULL
-    LIMIT 5
+    LIMIT 10
 """)
-for row in cur.fetchall():
-    print("author:", row["author"])
-    print("owner_responses:", row["owner_responses"])
-    print("---")
+
+rows = cur.fetchall()
+
+if not rows:
+    print("No reviews with owner responses found in the database.")
+
+for row in rows:
+    print("Author:             ", row["author"])
+    print("Review Date:        ", row["review_date"])
+    print("Owner Answer Date:  ", row["owner_response_date"])
+    print("Owner Answer Text:  ", row["owner_responses"])
+    print("-" * 50)
 
 con.close()
